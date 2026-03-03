d80d138bd0ddf4520c7a762535950dc31661af7d
pa azdevops <pa.azdevops@gmail.com>
2026-03-04 01:14:42 +0530

    Fixed SQL config for Azure

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const sql = require('mssql');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Parse connection string manually
+const connectionString = process.env.DefaultConnection;
+
 const config = {
-    connectionString: process.env.DefaultConnection
+    server: connectionString.match(/Server=tcp:(.*?),/)[1],
+    database: connectionString.match(/Initial Catalog=(.*?);/)[1],
+    user: connectionString.match(/User ID=(.*?);/)[1],
+    password: connectionString.match(/Password=(.*?);/)[1],
+    options: {
+        encrypt: true,
+        trustServerCertificate: false
+    }
 };
 
 app.get('/', (req, res) => {
@@ -15,7 +25,7 @@ app.get('/', (req, res) => {
 app.get('/data', async (req, res) => {
     try {
         await sql.connect(config);
-        const result = await sql.query`SELECT * FROM TestTable`;
+        const result = await sql.query('SELECT * FROM TestTable');
         res.json(result.recordset);
     } catch (err) {
         res.status(500).send(err.message);
