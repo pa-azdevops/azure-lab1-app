0307de5b35d776994601dbaeb0677ed775bff8d5
pa azdevops <pa.azdevops@gmail.com>
2026-03-04 00:29:53 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello Purnesh! Azure Lab 1 is working 🚀');
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
